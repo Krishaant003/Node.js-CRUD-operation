bfe79ef578eb7ce7802161ccf70dec34ccb75097
krishaant <krishaant2003@gmail.com>
2023-07-09 18:26:49 +0530

    Implementing input validation

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+const Joi = require('joi')
 const express = require('express');
 const app =  express()
 
@@ -13,6 +13,14 @@ const Genres = [
 ]
 
 
+validategenre = (genre) =>
+{
+    const schema = Joi.object({
+        category: Joi.string().min(3).required()
+    });
+    return schema.validate(genre)
+}
+ 
 app.get('/api/genres',(req,res)=>
 {
     res.send(Genres)
@@ -27,6 +35,9 @@ app.get('/api/genres/:id',(req,res)=>
 
 app.post('/api/genres',(req,res)=>
 {
+    const {error} = validategenre(req.body)
+    if(error) return res.status(404).send(error.details[0].message);
+
     const genre =
     {id:Genres.length +1,
      category: req.body.category
@@ -39,6 +50,9 @@ app.put('/api/genres/:id',(req,res)=>
 {
     const genre = Genres.find(c => c.id === parseInt(req.params.id))
     if(!genre) return res.status(400).send("No genre with this Id");
+
+    const {error} =validategenre(req.body);
+   if(error) return res.status(400).send(error.details[0].message);
 
     genre.category = req.body.category;
     res.send(genre);
@@ -59,3 +73,5 @@ app.listen(PORT,()=>
 {
     console.log(`Listening to port ${PORT}`);
 })
+
+
