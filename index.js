24a4ffe21de77156eff02916361fc5c4a4e982ca
krishaant <krishaant2003@gmail.com>
2023-07-09 17:27:31 +0530

    Creating Genres List

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const express = require('express');
+const app =  express()
+
+const Genres = [
+    {id : 1 , category: "Action"},
+    {id : 2 , category: "Romance"},
+    {id : 3 , category: "Anime"},
+    {id : 4 , category: "Thriller"},
+    {id : 5 , category: "Horror"}
+]
+
+
+app.get('/api/genres',(req,res)=>
+{
+    res.send(Genres)
+})
+
+app.get('/api/genres/:id',(req,res)=>
+{
+   const genre = Genres.find(c => c.id === parseInt(req.params.id))
+   if(!genre) return res.status(404).send("No genre available with this Id")
+   res.send(genre);
+})
+
+app.listen(3000,()=>
+{
+    console.log("Listening to port 3000");
+})
