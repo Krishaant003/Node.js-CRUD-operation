e7c65eaa9662812b7f24f1ac55acdab9634b08c0
krishaant <krishaant2003@gmail.com>
2023-07-20 21:17:55 +0530

    Added Validation for Customers

--- Routes/customers.js
+++ Routes/customers.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 const joi =require('joi');
+const Joi = require('joi');
 router.use(express.json());
 
 const customerSchema = new mongoose.Schema({
@@ -17,6 +18,16 @@ const customerSchema = new mongoose.Schema({
 });
 
 const Customer = new mongoose.model('Customer',customerSchema);
+
+validateCustomer = customer =>
+{
+    const schema = Joi.object({
+        name : Joi.string().min(3).required(),
+        phone: Joi.number().required()
+    })
+
+    return schema.validate(customer);
+}
 
 router.get('/', async (req ,res) => {
     const customers = await Customer.find().sort('name');
@@ -35,6 +46,9 @@ router.get('/:id', async (req,res) =>
 
 router.post('/' , async (req ,res) =>
 {
+    const {error} = validateCustomer(req.body);
+    if (error) return res.status(404).send(error.details[0].message);
+
     try {
     let customer = new Customer({
         name : req.body.name,
@@ -49,6 +63,10 @@ router.post('/' , async (req ,res) =>
 })
 router.put('/:id', async(req ,res) =>
 {
+    const {error} = validateCustomer(req.body);
+    if (error) return res.status(404).send(error.details[0].message);
+
+    
     try{
     const customer = await Customer.findByIdAndUpdate(req.params.id,{
         name: req.body.name,
